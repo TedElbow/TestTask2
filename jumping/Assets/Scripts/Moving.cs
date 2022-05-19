2da96c4e558e691f38c51e03df7627f5df8c8499
TedElbow <tedelbow@gmail.com>
2022-05-19 15:57:04 +0600

    12345
    12345

---
+++ jumping/Assets/Scripts/Moving.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    [SerializeField] private float _velocityValue;
+    [SerializeField] private bool FingerTap;
+    [SerializeField] private GameObject MainButton;
+
+    private void Start()
+    {
+        _velocityValue = 8; // минимальное придание ускорения, если игрок зажал палец и сразу отпустил
+    }
+    
+    private void FixedUpdate()
+    {
+        if (FingerTap == true) // проверка, зажат ли палец 
+        {
+            if(_velocityValue < 48) // ограничитель (чтобы персонаж не улетел за карту)
+            _velocityValue += Time.deltaTime*20; // оедленное наращивание силы
+        }
+        
+    }
+    public void FingerDown() // метод для отсеживания паоднятия пальца
+    {
+        FingerTap = true; 
+        this.GetComponent<Animator>().SetBool("Tapped", true);
+        this.GetComponent<Animator>().SetBool("Untapped", false);
+    }
+    public void FingerUp()// метод для зажатия  пальцем
+    {
+        MainButton.SetActive(false);// выключаем кнопку,чтобы игрок повторно не нажимал
+        FingerTap = false;
+        this.GetComponent<Animator>().SetBool("Tapped", false);
+        this.GetComponent<Animator>().SetBool("Untapped", true); 
+       
+    }
+
+    public void Jump() // скорость дается только после окончания анимации
+    {
+        this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, _velocityValue);
+    }
+
+}
---
+++ jumping/Assets/Scripts/PresentDetector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PresentDetector : MonoBehaviour
+{
+    private float _currentRange;
+    bool _moveStarted = false;
+    [SerializeField] private Text _presentsCount;
+    [SerializeField] private GameObject _endScreen;
+
+    private void Update()
+    {
+        if (_moveStarted)
+        {
+            if (this.GetComponent<Rigidbody>().velocity.z == 0)
+            {
+                _currentRange = this.transform.position.z;
+                WhatPresent();
+                _moveStarted = false;
+            }
+        }
+    }
+    private void WhatPresent()
+    {
+        int n = (int)_currentRange;
+        int a = n / 9;
+        if(a % 2 == 0)
+        {
+            int cubes = a + 2;
+            _presentsCount.text = cubes.ToString();
+            _endScreen.SetActive(true);
+        }
+        else
+        {
+            _presentsCount.text = ("ничего");
+            _endScreen.SetActive(true);
+        }
+    }
+    public void Jump() // скорость дается только после окончания анимации
+    {
+        _moveStarted = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
